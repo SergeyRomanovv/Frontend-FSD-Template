8b95d013876fa481f16b2423c31d18f6bc8729ed
Sergey Romanov <sergeyromanovv94@gmail.com>
2026-04-30 17:35:07 +0300

    refactor: удален jest-environment-jsdom, добавлен jsdom и кастомный environment

--- config/jest/jsdomEnvironment.js
+++ config/jest/jsdomEnvironment.js
@@ -1,8 +1,35 @@
-const { default: JSDOMEnvironment } = require('jest-environment-jsdom');
+const { default: NodeEnvironment } = require('jest-environment-node');
+const { JSDOM } = require('jsdom');
 
-module.exports = class CustomJSDOMEnvironment extends JSDOMEnvironment {
+module.exports = class CustomJSDOMEnvironment extends NodeEnvironment {
     async setup() {
         await super.setup();
+
+        this.dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
+            url: 'http://localhost',
+            pretendToBeVisual: true,
+            resources: 'usable',
+        });
+
+        const { window } = this.dom;
+        const { document } = window;
+
+        this.global.window = window;
+        this.global.document = document;
+        this.global.navigator = window.navigator;
+        this.global.location = window.location;
+        this.global.history = window.history;
+        this.global.Element = window.Element;
+        this.global.HTMLElement = window.HTMLElement;
+        this.global.localStorage = window.localStorage;
+        this.global.sessionStorage = window.sessionStorage;
+
+        this.global.fetch = window.fetch;
+        this.global.Request = window.Request;
+        this.global.Response = window.Response;
+        this.global.Headers = window.Headers;
+        this.global.URL = window.URL;
+
         const { TextEncoder, TextDecoder } = require('util');
         if (typeof this.global.TextEncoder === 'undefined') {
             this.global.TextEncoder = TextEncoder;
@@ -10,5 +37,10 @@ module.exports = class CustomJSDOMEnvironment extends JSDOMEnvironment {
         if (typeof this.global.TextDecoder === 'undefined') {
             this.global.TextDecoder = TextDecoder;
         }
+    }
+
+    async teardown() {
+        this.dom?.window?.close();
+        await super.teardown();
     }
 };
